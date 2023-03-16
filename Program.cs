d98cf6b6b089f14f3c22786e32f436e368473eef
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-03-16 14:43:49 +0100

    Add snack 2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿
-snack1();
+//snack1();
+snack2();
 
 void snack1()
 {
@@ -19,5 +20,27 @@ void snack1()
     else
     {
         Console.WriteLine("Il secondo numero è più grande!");
+    }
+}
+
+void snack2()
+{
+    Console.WriteLine("Inserisci la prima parola!");
+    var word1 = Console.ReadLine() ?? "";
+
+    Console.WriteLine("Inserisci la seconda parola!");
+    var word2 = Console.ReadLine() ?? "";
+
+    if (word1.Length > word2.Length)
+    {
+        Console.WriteLine("La prima parola è più lunga!");
+    }
+    else if (word1.Length == word2.Length)
+    {
+        Console.WriteLine("Le due parole hanno uguale lunghezza!");
+    }
+    else
+    {
+        Console.WriteLine("La seconda parola è più lunga!");
     }
 }

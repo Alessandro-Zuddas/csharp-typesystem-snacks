24e77bfa8809a802819bb3190a1bbed083fc6c6a
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-03-16 14:38:15 +0100

    Add snack 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
 ﻿
-Console.WriteLine("Hello, World!");
+snack1();
+
+void snack1()
+{
+    Console.WriteLine("Inserisci il primo numero!");
+    var num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Inserisci il secondo numero!");
+    var num2 = Convert.ToInt32(Console.ReadLine());
+
+    if(num1 > num2)
+    {
+        Console.WriteLine("Il primo numero è maggiore!");
+    }else if(num1 == num2)
+    {
+        Console.WriteLine("I due numeri sono uguali!");
+    }
+    else
+    {
+        Console.WriteLine("Il secondo numero è più grande!");
+    }
+}
